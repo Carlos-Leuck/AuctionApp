c10242d5731761c9b2550faab1e43c33c32d7fbb
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-12-27 14:53:15 -0300

    Using postman for integration tests

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-
-namespace AuctionService.IntegrationTests.Fixtures;
-
-public class CustomWebAppFactory : WebApplicationFactory<Program>
-{
-
-}
