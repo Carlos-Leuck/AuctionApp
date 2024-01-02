24eac19e327c8af437ed31eae8ffd769b67403fa
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2024-01-02 17:30:49 -0300

    Add Zustand for state management

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -3,11 +3,10 @@
 import { Auction, PagedResult } from '@/types'
 
 export async function getData(
-  pageNumber: number,
-  pageSize: number
+ query: string
 ): Promise<PagedResult<Auction>> {
   const res = await fetch(
-    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
+    `http://localhost:6001/search${query}`
   )
 
   if (!res.ok) throw new Error('Failed to fetch data')
