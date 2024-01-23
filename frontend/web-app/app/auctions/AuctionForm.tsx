4e696aa1063192849e99ceea3cd5066e4dd994c6
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2024-01-23 18:58:22 -0300

    Auction form created

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -5,21 +5,54 @@ import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input'
 import DateInput from '../components/DateInput'
+import { createAuction, updateAuction } from '../actions/auctionActions'
+import { usePathname, useRouter } from 'next/navigation'
+import toast from 'react-hot-toast'
+import { Auction } from '@/types'
 
-export default function AuctionForm() {
+type Props = {
+  auction?: Auction
+}
+
+export default function AuctionForm({ auction }: Props) {
+  const router = useRouter()
+  const pathname = usePathname()
   const {
     control,
     handleSubmit,
     setFocus,
-    formState: { isSubmitting, isValid, isDirty, errors }
+    reset,
+    formState: { isSubmitting, isValid }
   } = useForm({ mode: 'onTouched' })
 
   useEffect(() => {
+    if (auction) {
+      const { make, model, color, mileage, year } = auction
+      reset({ make, model, color, mileage, year })
+    }
     setFocus('make')
-  }, [setFocus])
+  }, [setFocus, reset, auction])
 
-  function onSubmit(data: FieldValues) {
-    console.log(data)
+  async function onSubmit(data: FieldValues) {
+    try {
+      let id = ''
+      let res
+      if (pathname === '/auctions/create') {
+        res = await createAuction(data)
+        id = res.id
+      } else {
+        if (auction) {
+          res = await updateAuction(data, auction.id)
+          id = auction.id
+        }
+      }
+      if (res.error) {
+        throw res.error
+      }
+      router.push(`/auctions/details/${id}`)
+    } catch (error: any) {
+      toast.error(error.status + ' ' + error.message)
+    }
   }
 
   return (
@@ -60,6 +93,7 @@ export default function AuctionForm() {
         />
       </div>
 
+      {pathname === '/auctions/create' && 
       <>
         <Input
           label="Image URL"
@@ -86,6 +120,7 @@ export default function AuctionForm() {
           />
         </div>
       </>
+      }
 
       <div className="flex justify-between">
         <Button outline color="gray">
@@ -93,6 +128,7 @@ export default function AuctionForm() {
         </Button>
         <Button
           isProcessing={isSubmitting}
+          disabled={!isValid}
           type="submit"
           outline
           color="success"
